81d0dd9553a61512fdb0e7db8a25e209f7ad4733
Roy Bass <roybass@gmail.com>
2021-02-07 22:32:20 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+let numItems = 5;
+
+let selected = getRandomInt(numItems);
+
+function select() {
+  selected = getRandomInt(numItems)
+  ani();
+  setTimeout(ani, 1000);
+}
+
+function onClickDiv(idx) {
+  let text = (idx === selected) ? "Impostor" : "Crewmate"
+  window.alert(text);
+}
+
+function ani(){
+  let boxes = document.getElementsByClassName('box')
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].className = toggle(boxes[i].className, 'spinning');
+  } 
+}
+
+function toggle(str1, str2) {
+  if (str1.indexOf(str2) == -1) {
+    return str1 + " " + str2;
+  } else {
+    return str1.replace(str2, "").trim();
+  }
+}
