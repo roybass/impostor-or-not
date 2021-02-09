69979152475d55f43dacef9c34814197fd745bbe
Roy Bass <roybass@gmail.com>
2021-02-09 22:30:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
+const defaultColors = ['Red', 'Green', 'Yellow', 'Blue', 'White'];
+
+const allColors = ['Red', 'Green', 'Yellow', 'Blue', 'White', 'Purple', 'Aqua', 'Gold', 'Hotpink'];
+
+const checks = document.getElementById('checks');
+for (let i = 0; i < allColors.length; i++) {
+  const input = document.createElement('input');
+  input.type = "checkbox";
+  input.id = "check-" + i;
+  input.className = "inputCheckbox";
+  input.onclick = () => {updateBoxes();};
+  input.checked = defaultColors.indexOf(allColors[i]) >= 0;
+  
+  const label = document.createElement('label');
+  label.for = input.id;
+  label.style = "color:" + allColors[i] + ";";
+  label.innerHTML = allColors[i];
+  
+  checks.appendChild(input);
+  checks.appendChild(label);
+  checks.appendChild(document.createElement('br'));
+}
+
+let boxes = [];
+
+function updateBoxes() {
+  boxes = [];
+  for (let i = 0; i < allColors.length; i++) {
+      let checkbox = document.getElementById("check-" + i);
+      if (checkbox.checked === true) {
+        boxes.push(allColors[i]);
+      }
+  }
+  const container = document.getElementById("container");
+  container.innerHTML = '';
+  for (let i = 0; i < boxes.length; i++) {
+    const div = document.createElement('div');
+    div.className="box";
+    div.onclick = () => { onClickDiv(i); }
+    div.style = "background:" + boxes[i] + ";"
+    container.appendChild(div);
+  }
+  select();
+}
+
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-let numItems = 5;
-
-let selected = getRandomInt(numItems);
+let selected = getRandomInt(boxes.length);
 
 function select() {
-  selected = getRandomInt(numItems)
+  selected = getRandomInt(boxes.length)
   ani();
   setTimeout(ani, 1000);
 }
@@ -33,3 +77,6 @@ function toggle(str1, str2) {
     return str1.replace(str2, "").trim();
   }
 }
+
+
+updateBoxes();
